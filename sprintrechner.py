893a1309562c9aa5957f314d577e5a915befcc30
Khati <59515899+kadirhatip@users.noreply.github.com>
2022-05-12 11:47:03 +0200

    Grundgerüst

--- sprintrechner.py
+++ sprintrechner.py
@@ -1,4 +1,16 @@
-print("Hallo Welt")
-print("Was geht")
-print("Ich pushe wie ein Dealer")
-print("Hat das geklappt")
+import tkinter as tk
+
+window = tk.Tk()
+
+greeting_lbl = tk.Label(window, text="Hallo Welt")
+greeting_lbl.pack()
+
+
+
+#Um die GUI scharfzustellen und sie dann zu starten
+try:
+    from ctypes import windll
+
+    windll.shcore.SetProcessDpiAwareness(1)
+finally:
+    window.mainloop()
