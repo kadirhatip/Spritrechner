998ec9dd0ff650d8cda687371a7c7a02cfccca2f
Khati <59515899+kadirhatip@users.noreply.github.com>
2022-05-19 21:01:16 +0200

    Converter fertig

--- sprintrechner.py
+++ sprintrechner.py
@@ -1,11 +1,10 @@
 import PySimpleGUI as sg
 
-title = "Mein erstes Fenster"
+title = "Converter"
 
 layout = [
-    [sg.Text("Gib deinen Namen ein und drücke den Knopf")],
-    [sg.InputText(key="_input"), sg.Button("Knopf", key="_knopf")],
-    [sg.Text("Ausgabe", key="_ausgabe")]
+    [sg.InputText(key = "_input"), sg.Spin(["km to mile", "kg to pound", "sec to min"], key= "_spin"), sg.Button("Convert", key = "_button")],
+    [sg.Text("Output", key = "_output")]
 ]
 
 window = sg.Window(title, layout)
@@ -16,5 +15,17 @@ while True:
     if event == sg.WINDOW_CLOSED:
         break
 
-    if event == "_knopf":
-        window.find_element("_ausgabe").update(("Hallo %s" % values["_input"]))
+    if event == "_button":
+        input = window.find_element("_input").get()
+        spin = window.find_element("_spin").get()
+
+        if (spin == "km to mile"):
+            factor = 0.621
+        elif (spin == "kg to pound"):
+            factor = 2.205
+        elif (spin == "sec to min"):
+            factor = 1/60
+        
+        solution = int(input) * factor
+    
+        window.find_element("_output").update(solution)
