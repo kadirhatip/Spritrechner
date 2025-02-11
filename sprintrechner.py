cd7bf2c549be65d71be09155a153920d430e70fd
Khati <59515899+kadirhatip@users.noreply.github.com>
2025-02-11 09:27:13 +0100

    Schon lange her

---
+++ sprintrechner.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+breite = 600
+höhe = 400
+rand_oben = 50
+rand_seite = 50
+seitenmaße = f"{breite}x{höhe}+{rand_oben}+{rand_seite}"
+
+fenster = tk.Tk()
+fenster.title("Spritrechner")
+fenster.geometry(seitenmaße)
+
+begrüßung_lbl = tk.Label(fenster, text="Hallo Welt")
+begrüßung_lbl.pack()
+
+
+
+
+#Um die GUI scharfzustellen und sie dann zu starten
+try:
+    from ctypes import windll
+
+    windll.shcore.SetProcessDpiAwareness(1)
+finally:
+    fenster.mainloop()
